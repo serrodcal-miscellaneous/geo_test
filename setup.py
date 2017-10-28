5580b4155675d9b9ffc476b71110207188d61e2d
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-10-28 23:55:54 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='geo_test',
+    version='0.0.1',
+    url='https://github.com/serrodcal/geo_test',
+    license='MIT License',
+    author='Sergio Rodriguez',
+    author_email='sergiorodriguezcalvo@gmail.com',
+    keywords='Geo Test',
+    packages=find_packages(),
+    install_requires=[],
+    test_suite='tests',
+)
